01853932d7aad5d020822a89f1ca39e293e24df1
Sergii Butrii <sergiibutrii555@gmail.com>
2025-03-31 00:35:27 +0200

    sigo con el tailwindcss

--- OpenWebinars/Angular/proyectos/shopapp/src/app/components/about-us/about-us.component.ts
+++ OpenWebinars/Angular/proyectos/shopapp/src/app/components/about-us/about-us.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ProveService } from '../../services/sesion.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent implements OnInit {
   public userData: any;
-  
-  constructor(private proveService: ProveService){}
+  public isAboutView: boolean = true;
+  public isHomeColor = '';
+  public backroundfImg= 'https://images.ctfassets.net/ihx0a8chifpc/4Yp1F82NF8yN9gUHXMphNz/c254302efb588196d9a607832cb24e28/lorem-picsum-1280x720.jpg?w=1920&q=60&fm=webp';
+ 
+  constructor(private proveService: ProveService, private router: Router){
+    
+    this.router.events.subscribe(() => {
+      this.isAboutView = this.router.url === '/home/about-us';
+      this.isHomeColor = this.isAboutView ? 'text-white' : 'text-black';
+    });
+  }
 
   ngOnInit(): void {
     this.proveService.userData$.subscribe(
@@ -19,5 +31,8 @@ export class AboutUsComponent implements OnInit {
       }
     );
     console.log(this.userData);
+  }
+  onClick(){
+    this.router.navigate([`/home`]);
   }
 }
--- OpenWebinars/Angular/proyectos/shopapp/src/app/components/home-about-us/home-about-us.module.ts
+++ OpenWebinars/Angular/proyectos/shopapp/src/app/components/home-about-us/home-about-us.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AboutUsComponent } from '../about-us/about-us.component';
 import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [AboutUsComponent],
+  declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forChild([{path: '', component: AboutUsComponent}])
   ]
 })
